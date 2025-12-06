612d693be538f48e5230fbf7ed79eeef7c65b6e6
FahadBinJunaid <160081833+FahadBinJunaid@users.noreply.github.com>
2025-12-06 23:40:19 +0500

    Error Fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/introduction-to-physical-ai">
+            to="/docs/intro">
             Start Learning
           </Link>
         </div>
